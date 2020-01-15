7531594d55a7faf6e5bf71124a2cc4c2338aa812
Tamara J Cheruiyot <110552@STD.STRATHMORE.LOCAL>
2020-01-15 20:33:25 +0300

    My first commit

---
+++ lab1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method= "post">
+     <table align = "center">
+      <tr>
+         <td><input type = "text" name = "first_name" required placeholder= "First Name" /></td></tr>
+      <tr>
+         <td><input type = "text" name = "last_name" required placeholder= "Last Name" /></td></tr>
+      <tr>
+         <td><input type = "text" name = "city_name" required placeholder= "City" /></td></tr>
+      <tr>
+         <td><button type = "submit" name = "btn-save"> <strong>SAVE</strong> /button> </td></tr>
+      </table>
+    </form>
+</body>
+</html>
