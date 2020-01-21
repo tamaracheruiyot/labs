463d13404f14c6ed72f4b86039070b62bd7c1fd5
tamaracheruiyot <40488945+tamaracheruiyot@users.noreply.github.com>
2020-01-21 11:53:06 -0800

    Update lab1.php

--- lab1.php
+++ lab1.php
@@ -1,23 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <form method= "post">
-     <table align = "center">
-      <tr>
-         <td><input type = "text" name = "first_name" required placeholder= "First Name" /></td></tr>
-      <tr>
-         <td><input type = "text" name = "last_name" required placeholder= "Last Name" /></td></tr>
-      <tr>
-         <td><input type = "text" name = "city_name" required placeholder= "City" /></td></tr>
-      <tr>
-         <td><button type = "submit" name = "btn-save"> <strong>SAVE</strong> /button> </td></tr>
-      </table>
-    </form>
-</body>
+<?php
+
+include_once 'DBConnector.php';
+include_once 'user.php';
+
+$con = new DBConnector;
+
+if (isset ($_POST ['btn-save'])){
+    $first_name = $_POST['$first_name'];
+    $last_name = $_POST['$last_name'];
+    $city = $_POST['$city_name'];
+    
+$user = new user ($first_name, $last_name, $city);
+$res = $user->save();
+
+if ($res){
+    echo "Save Operation was Successful";
+}else{
+    echo "An error occurred";
+}
+}
+?>
+<html>
+    <head>
+        <title>Tamara's LAB 1</title>
+    </head>
+    <body>
+        <form method= "post">
+            <table align = "center">
+                <tr>
+                    <td><input type = "text" name = "first_name" required placeholder= "First Name" /></td>
+                </tr>
+                <tr>
+                    <td><input type = "text" name = "last_name" placeholder= "Last Name" /></td>
+                </tr>
+                <tr>
+                    <td><input type = "text" name = "city_name" placeholder= "City" /></td>
+                </tr>
+                <tr>
+                    <td><button type = "submit" name = "btn-save"><strong>SAVE</strong> /button> </td>
+                </tr>
+            </table>
+        </form>
+    </body>
 </html>
